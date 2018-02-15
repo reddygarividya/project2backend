90f285b5e09d6462941db303704a7a5e10cd554b
Test <Test@DESKTOP-J1B7EKC>
2018-02-15 02:45:02 -0700

    register and login

---
+++ src/main/java/com/niit/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.niit.dao;
+
+
+import com.niit.model.User;
+
+public interface UserDao {
+
+	 void registerUser(User user);
+	 boolean isEmailUnique(String email);
+	 User login(User user);
+		
+	
+	
+}
