21b8fd41e10235b7407c7fcb996679b7862c4292
Test <Test@DESKTOP-J1B7EKC>
2018-03-13 01:06:26 -0700

    profile

--- src/main/java/com/niit/configuration/DBConfiguration.java
+++ src/main/java/com/niit/configuration/DBConfiguration.java
@@ -16,6 +16,7 @@ import com.niit.model.BlogPost;
 import com.niit.model.BlogPostLikes;
 import com.niit.model.Job;
 import com.niit.model.Notification;
+import com.niit.model.ProfilePicture;
 import com.niit.model.User;
 
 @Configuration
@@ -34,7 +35,7 @@ public class DBConfiguration {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,Notification.class,BlogPostLikes.class,BlogComment.class};//class objects of all entities
+		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,Notification.class,BlogPostLikes.class,BlogComment.class,ProfilePicture.class};//class objects of all entities
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	@Bean
---
+++ src/main/java/com/niit/dao/ProfilePictureDao.java
@@ -0,0 +1,12 @@
+package com.niit.dao;
+
+import com.niit.model.ProfilePicture;
+
+public interface ProfilePictureDao {
+	void uploadProfilePicture(ProfilePicture profilePicture);
+	ProfilePicture getProfilePic(String email);
+	
+}
+	
+
+
